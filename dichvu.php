86df56af2130d8fbdf6626c6d7529e6bfaa9d469
SonML <ledinhson209@gmail.com>
2021-10-01 17:31:28 +0700

    update code

---
+++ dichvu.php
@@ -0,0 +1,25 @@
+<?php include 'header.php';?>
+<main>
+	<section class="bread-banner">
+		<div class="avr-bread"><img src="images/bread.png" class="img-fluid w-100" alt=""></div>
+		<div class="cap-bread">
+			<div class="container">
+				<div class="info-bread">
+					<h2>Dịch vụ</h2>
+					<p>Nhà bạt & Sự kiện</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="box-service">
+		<div class="container">
+			<div class="list-service">
+				<div class="row">
+					<div class="col-md-4"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+      
