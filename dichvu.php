a6208c3fc3f63471ed3ff6904e0c8653eaccba09
sonledinh <sonledinh@admicro.vn>
2021-10-01 23:10:22 +0700

    up pages

--- dichvu.php
+++ dichvu.php
@@ -15,7 +15,143 @@
 		<div class="container">
 			<div class="list-service">
 				<div class="row">
-					<div class="col-md-4"></div>
+					<div class="col-md-3">
+						<div class="sidebar-cate">
+							<div class="box-bar-cate">
+								<div class="box-bar-logo text-center">
+									<img src="images/logo-bar.png" class="img-fluid" alt="">
+									<p>Chuyên nghiệp - Trung thực - Uy tín</p>
+								</div>
+							</div>
+							<div class="box-bar-cate">
+								<div class="link-box-bar">
+									<ul>
+										<li><a href="" class="active">Cho thuê nhà bạt</a></li>
+										<li><a href="">Tổ chức sự kiện</a></li>
+										<li><a href="">Hội chợ triển lãm</a></li>
+										<li><a href="">Mua bán nhà bạt</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-9">
+						<div class="list-srvice">
+							<div class="row">
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="item-srvice">
+										<div class="avarta"><a href=""><img src="images/srv.png" class="img-fluid" alt=""></a></div>
+										<div class="info-abs">
+											<div class="info">
+												<h3><a href="">Nhà bạt sự kiện</a></h3>
+												<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="pagination w-100">
+										<ul class="page-numbers">
+											<li><span aria-current="page" class="page-numbers current">1</span></li>
+											<li><a class="page-numbers" href="">2</a></li>
+											<li><a class="page-numbers" href="">3</a></li>
+											<li><a class="page-numbers" href="">4</a></li>
+											<li><span class="page-numbers dots">…</span></li>
+											<li><a class="next page-numbers" href="">Cuối</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
---
+++ project.php
@@ -0,0 +1,74 @@
+<?php include 'header.php';?>
+<main>
+	<section class="bread-banner">
+		<div class="avr-bread"><img src="images/bn-project.png" class="img-fluid w-100" alt=""></div>
+	</section>
+	<section class="box-service">
+		<div class="container">
+			<div class="list-project-cate">
+				<div class="item-project">
+					<div class="row">
+						<div class="col-md-5">
+							<div class="txt-project">
+								<div class="info-pj">
+									<h3 class="text-center"><a href="">Hội chợ tháng khuyến mãi sân vận động phú thọ</a></h3>
+									<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="avarta-pj"><img src="https://nguyenbinh.com/thumb/1366x650/1/80/upload/hinhanh/slide1631770646.png" class="img-fluid w-100" alt=""></div>
+						</div>
+					</div>
+				</div>
+				<div class="item-project">
+					<div class="row">
+						<div class="col-md-5">
+							<div class="txt-project">
+								<div class="info-pj">
+									<h3 class="text-center"><a href="">Hội chợ tháng khuyến mãi sân vận động phú thọ</a></h3>
+									<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="avarta-pj"><img src="https://nguyenbinh.com/thumb/1366x650/1/80/upload/hinhanh/slide1631770646.png" class="img-fluid w-100" alt=""></div>
+						</div>
+					</div>
+				</div>
+				<div class="item-project">
+					<div class="row">
+						<div class="col-md-5">
+							<div class="txt-project">
+								<div class="info-pj">
+									<h3 class="text-center"><a href="">Hội chợ tháng khuyến mãi sân vận động phú thọ</a></h3>
+									<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="avarta-pj"><img src="https://nguyenbinh.com/thumb/1366x650/1/80/upload/hinhanh/slide1631770646.png" class="img-fluid w-100" alt=""></div>
+						</div>
+					</div>
+				</div>
+				<div class="item-project">
+					<div class="row">
+						<div class="col-md-5">
+							<div class="txt-project">
+								<div class="info-pj">
+									<h3 class="text-center"><a href="">Hội chợ tháng khuyến mãi sân vận động phú thọ</a></h3>
+									<div class="desc">Một số nhà bạt không gian tiêu chuẩn sử dụng trong sự kiện outdoor Trong tổ chức sự kiện, đặc biệt là các sự kiện chịu ảnh hưởng của yếu tố thời tiết, một trong các hạng mục gần như bắt buộc phải có là những chiếc nhà có mái che - nhà bạt không gian tiêu chuẩn. Hình ảnh của những chiếc nhà bạt gần như là đại diện về mặt sản phẩm của các agency trong ngành tổ chức sự kiện. Trong khuôn khổ bài viết này</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="avarta-pj"><img src="https://nguyenbinh.com/thumb/1366x650/1/80/upload/hinhanh/slide1631770646.png" class="img-fluid w-100" alt=""></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+      
